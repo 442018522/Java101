8721c383c31d23cc8d5d31aaf891ac8653d67434
Hani M. Th <hmthubaiti@uqu.edu.sa>
2021-09-30 00:06:59 +0300

    solve the program

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 //Your Name asim nabil almuayqili
 //Your ID 442018522
+//your grade is 0/5
 class Main {
   public static void main(String[] args) {
       //Start your code 
