d1e94280e788b3e637222d6209f86fac09dbfb9d
A.N <91271265+442018522@users.noreply.github.com>
2021-12-14 08:11:21 +0300

    Create NegativeAndPositive.java

---
+++ NegativeAndPositive.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;,
+
+public class NegativeAndPositive {
+
+public static void main(String[] args) {
+
+Scanner scan = new Scanner(System.in);
+
+float var ;
+
+System.out.println("Entar a Number ");
+
+var = scan.nextFloat();
+
+if (var < 0) {
+
+System.out.println("negative");
+
+}else{
+
+System.out.println("positive");
+
+}
+
+}
+
+}
